60d7471459b38251923ab0ca67d40cef2e059cad
Oluwadamilola Oduntan <oduntandamilola@gmail.com>
2024-06-24 23:38:33 -0400

    Added requirements.txt and config.py

---
+++ jobs/config.py
@@ -0,0 +1,4 @@
+configuration = {
+    "AWS_ACCESS_KEY": "AKIA0BXXHQDDVX0G5Y7B",
+    "AWS_SECRET_KEY": "c4hkzd1R+7b21WJ29wrt317guriJdquOK/WlLoeZ"
+}
--- jobs/spark-city.py
+++ jobs/spark-city.py
@@ -124,4 +124,8 @@ def main():
 if __name__ == "__main__":
     main()
 
-# docker exec -it spark-master spark-submit --master spark://spark-master:7077 --packages org.apache.spark:spark-sql-kafka-0-10_2.13:3.5.0,org.apache.hadoop:hadoop-aws:3.3.1,com.amazonaws:aws-java-sdk:1.11.469 jobs/spark-city.py
+
+# python3 -m venv "virtual env name"
+# pip install requirements.txt
+# docker-compose up -d
+# docker exec -it spark-master spark-submit --master spark://spark-master:7077 --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.1,org.apache.hadoop:hadoop-aws:3.3.1,com.amazonaws:aws-java-sdk:1.11.469 jobs/spark-city.py
